fb7e3479018976e08bfdb23915fb549e1b985a5d
Filipe <83041450+filipe1238@users.noreply.github.com>
2022-09-29 14:45:44 -0300

    update for filter

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,15 +1,49 @@
 import React from 'react'
 import RoomsFilter from './RoomFilter'
 import RoomsList from './RoomList'
+import { withRoomConsumer } from '../context'
+import Loading from './Loading'
+
+function RoomsContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />
+  }
+  return (
+    < div >
+      <RoomsFilter rooms={rooms} />
+      <RoomsList rooms={sortedRooms} />
+    </div>)
+}
+
+export default withRoomConsumer(RoomsContainer)
+
+
+
+/* import React from 'react'
+import RoomsFilter from './RoomFilter'
+import RoomsList from './RoomList'
+import { RoomConsumer } from '../context'
+import Loading from './Loading'
 
 function RoomsContainer() {
   return (
-    <div>
-      RoomsContainer
-      <RoomsFilter />
-      <RoomsList />
-    </div>
+    <RoomConsumer>
+      {
+        (value) => {
+          const { loading, sortedRooms, rooms } = value;
+          if (loading) {
+            return <Loading />
+          }
+          return (< div >
+            <RoomsFilter rooms={rooms} />
+            <RoomsList room={sortedRooms} />
+          </div>)
+        }
+      }
+    </RoomConsumer >
   )
+
 }
 
-export default RoomsContainer
+export default RoomsContainer */
--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import { useContext } from 'react'
+import { RoomContext } from '../context'
+import Title from '../components/Title'
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))]
+}
+function RoomsFilter({ rooms }) {
 
-function RoomsFilter() {
+  const context = useContext(RoomContext)
+  const {
+    handleChange, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast, pets
+  } = context;
+  let types = getUnique(rooms, 'type');
+  types = ['all', ...types]
+  types = types.map((item, index) => {
+    return <option value={item} key={index} >{item}</option>
+  })
   return (
-    <div>RoomsFilter</div>
+    <section className='filter-container'>
+      <Title title="Search room" />
+      <form className='filter-form'>
+        {/* select type */}
+        <div className='form-group'>
+          <label htmlFor='type'>room type</label>
+          <select
+            name='type'
+            id='type'
+            value={type}
+            className='form-control'
+            onChange={handleChange}>
+            {types}
+          </select>
+
+        </div>
+        {/*end of select type  */}
+
+      </form>
+    </section>
   )
 }
 
